59d1e0029ed7904b67f3f0b8f7df956eff9d816f
Angelo <akyjoon@gmail.com>
2018-01-19 23:49:14 +0100

    dat

--- config/database.js
+++ config/database.js
@@ -1,14 +1,5 @@
-// if(process.env.NODE_ENV == 'production') {
-//   module.exports = {mongoURI: 'mongodb://akyjoon:Qangelo1232@ds151451.mlab.com:51451/monitors'}
-// } else {
-//   module.exports = {mongoURI: 'mongodb://localhost/studio-monitors'}
-// }
-
-
-
-
 if(process.env.NODE_ENV == 'production') {
   module.exports = {mongoURI: 'mongodb://akyjoon:Qangelo1232@ds151451.mlab.com:51451/monitors'}
 } else {
-  module.exports = {mongoURI: 'mongodb://akyjoon:Qangelo1232@ds151451.mlab.com:51451/monitors'}
+  module.exports = {mongoURI: 'mongodb://localhost/studio-monitors'}
 }
