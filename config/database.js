ec8263a6eee2e937ea8f81ec23c91c5ac541c217
akyjoon <akyjoon@gmail.com>
2018-01-20 09:55:37 +0100

    Delete database.js

--- config/database.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == 'production') {
-  module.exports = {mongoURI: 'mongodb://akyjoon:Qangelo1232@ds151451.mlab.com:51451/monitors'}
-} else {
-  module.exports = {mongoURI: 'mongodb://localhost/studio-monitors'}
-}
