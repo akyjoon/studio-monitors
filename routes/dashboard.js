51d95d8db718feb108ddbe8ae79ff7d96b4bd4e6
Angelo <akyjoon@gmail.com>
2018-01-22 06:50:24 +0100

    user profile - user data made responsive

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose');
 const path = require('path')
 const async = require('async');
 const fs = require('fs');
-const multer  = require('multer');
+const multer = require('multer');
 const bodyParser = require('body-parser');
 
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended: true}))
+router.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 //Load Monitor model
 require('../models/Monitors');
@@ -16,26 +18,56 @@ const Monitor = mongoose.model('monitors');
 require('../models/User');
 const User = mongoose.model('users');
 
-//Get Dashboard my monitors tab
-router.get('/', function(req, res, next) {
-  //Check if user is logged in
-  if(req.isAuthenticated()) {
+//Get Dashboard
+
+// router.get('/', (req,res,next) => {
+//   if(req.isAuthenticated()) {
+//     return next();
+//   }
+//   res.redirect('/')
+// }, (req, res) => {
+//   Monitor.find({
+//     user: req.user.id
+//   }, (err, userMonitor) => {
+//     Monitor.find({
+//       'reviews.reviewUser': req.user.id
+//     }, (err, userReviews) => {
+//       // console.log('user Monitor', userMonitor);
+//       console.log('user reviews', userReviews);
+//       res.render('./dashboard/dashboard', {
+//         userMonitor: userMonitor,
+//         userReviews: userReviews
+//       })
+//     })
+//   })
+// })
+
+//test2
+router.get('/', (req, res, next) => {
+  if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/')
 }, (req, res) => {
-    Monitor.find({
-      user:req.user.id
+  Monitor.find({
+      user: req.user.id
     })
-      .populate('user')
-      .populate('reviews.reviewUser')
-      .then(monitors => {
-        res.render('./dashboard/dashboard', {
-          monitors: monitors
-        });
-      });
-
+    .populate('user')
+    .then((userMonitor) => {
+      Monitor.find({
+          'reviews.reviewUser': req.user.id
+        })
+        .populate('user')
+        .then((userReviews) => {
+          res.render('./dashboard/dashboard', {
+            userMonitor: userMonitor,
+            userReviews: userReviews
+          })
+        })
+    })
 })
+
+
 //set multer for profile image storage
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -45,15 +77,17 @@ var storage = multer.diskStorage({
     cb(null, file.originalname)
   }
 })
- 
-var upload = multer({ storage: storage })
+
+var upload = multer({
+  storage: storage
+})
 
 //add profile image for a user
 router.put('/avatar/:id', upload.single('avatar'), (req, res, next) => {
 
   User.findOne({
-    _id: req.params.id
-  })
+      _id: req.params.id
+    })
     .then(user => {
       user.avatar = req.file.originalname
 
