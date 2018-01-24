684dd77ed90c4e93e596a7466a7547c3cc01b28a
Angelo <akyjoon@gmail.com>
2018-01-24 06:45:19 +0100

    minor fixes

--- config/database.js
+++ config/database.js
@@ -1,10 +1,3 @@
-// if(process.env.NODE_ENV == 'production') {
-//   module.exports = {mongoURI: 'mongodb://akyjoon:Qangelo1232@ds151451.mlab.com:51451/monitors'}
-// } else {
-//   module.exports = {mongoURI: 'mongodb://localhost/studio-monitors'}
-// }
-
-
 if(process.env.NODE_ENV == 'production') {
   module.exports = require('./keys_prod')
 } else {
